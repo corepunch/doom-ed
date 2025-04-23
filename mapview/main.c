dda71e1fe433009415626085706283c3dbb57dd8
Igor Chernakov <igor.chernakov@gmail.com>
2025-04-23 10:36:05 +0200

    Initial Commit

---
+++ mapview/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mapview
+//
+//  Created by Igor Chernakov on 23.04.25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
